3b0e4119b58cdb0cf53feefb4575152c51e1b984
Andres Diaz <afdiaz2@gmail.com>
2016-09-07 19:32:30 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#Excercises for the course Machine Learning by Stanford
+
+The exercises correspond to the course available through Coursera from September through November 2016.
+
+These are my solutions to the programming assignments.
+
+##Week 2 Linear Regression with Multiple Variables & Octave/Matlab Tutorial
+
+This week, we calculated the profit of a food truck company based on the data of profits each food truck has in different cities and their corresponding populations.
+
+The mandatory exercises were of gradient descent with one feature and the optional ones have multiple features.
+
+I solved the gradient descent with one feature doing an iteration over the sum of the prediction deviations and then over the number of features and then over the number of iterations that actually gradient descent run through.
+
+![Gradient Descent algorithm](http://tinypic.com/r/2mwe7wl/9)
+
+Before that, I had to calculate the cost function which I did using the vectorized method.
+
+![Cost Function](http://tinypic.com/r/8znsm1/9)
---
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -0,0 +1,41 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta.
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+    number_of_thetas = length(theta);
+    new_theta = zeros(number_of_thetas,1);
+    for j = 1:number_of_thetas
+      sum_of_prediction_deviations = 0;
+      for i = 1:m
+        %h(x)=X*theta
+        hx = X(i,:) * theta;
+        prediction_deviation = (hx - y(i,:))*X(i,j);
+        sum_of_prediction_deviations = sum_of_prediction_deviations + prediction_deviation;
+      end
+      new_theta(j) = theta(j) - (alpha/m)*sum_of_prediction_deviations;
+    end
+
+    theta = new_theta;
+
+    % Save the cost J in every iteration
+    cost = computeCost(X, y, theta);
+    sprintf('Gradient: %d Cost: %d',iter,cost)
+    J_history(iter) = cost;
+
+end
+
+end
